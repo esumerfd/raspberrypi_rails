981834e1bebd42de52d35bc8bdead04e001d51d2
Ed Sumerfield <esumerfd@bitbashers.org>
2013-02-09 18:42:08 -0500

    Initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pipi::Application.config.secret_token = '72cfb456744061615c65f2b8204a97196379ee6b54ac8da80061119f2acfaeb6bd4d6c3aa83a1f326dd6b90885d4b7a9d15b69b034d37ecc2e130a762f7cb988'
